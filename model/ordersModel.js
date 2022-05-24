2f05d5e090f77079678c8af09b84da8b034e77d5
Aniket Atram <60972814+AniketAtram@users.noreply.github.com>
2022-05-24 15:39:16 +0530

    Created orders schema

--- model/ordersModel.js
+++ model/ordersModel.js
@@ -1,9 +1,23 @@
 const mongoose = require('mongoose');
 const ordersSchema = mongoose.Schema({
-    amount: {},
-    order_date: {},
-    product_id: {},
-    shipping_address_id: {},
+    address: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Address',
+        required: true
+    },
+    product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product',
+        required: true
+    },
+    quantity: {
+        type: Number,
+        required: true
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
 });
-const Orders = mongoose.model('Orders', productsSchema);
+const Orders = mongoose.model('Orders', ordersSchema);
 module.exports = Orders;
