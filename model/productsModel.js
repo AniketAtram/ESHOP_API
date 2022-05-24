e650259d9a8f351bde719a5877a46bb58f1dfb0c
Aniket Atram <60972814+AniketAtram@users.noreply.github.com>
2022-05-24 15:46:09 +0530

    Created products schema

--- model/productsModel.js
+++ model/productsModel.js
@@ -1,14 +1,41 @@
 const mongoose = require('mongoose');
 const productsSchema = mongoose.Schema({
-    availableItems: {},
-    category: {},
-    created: {},
-    description: {},
-    image_url: {},
-    manufacturer: {},
-    name: {},
-    price: {},
-    updated: {}
+    name: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    manufacturer: {
+        type: String,
+        required: true
+    },
+    availableItems: {
+        type: Number,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    imageURL: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now()
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    }
 });
 const Products = mongoose.model('Products', productsSchema);
 module.exports = Products;
